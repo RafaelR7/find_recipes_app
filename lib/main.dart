0b4ada38485e64764ef7caf8406738c46b66322f
Rafael Honda <RafaHonda@MacBook-Pro-de-HONDA.local>
2020-05-30 21:56:45 -0300

    portrait mode only & layout adjusts

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:find_recipes/ui/home/home.dart';
 import 'package:find_recipes/ui/styles/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MyApp());
+  // runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(new MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
--- lib/ui/recipes/recipes.dart
+++ lib/ui/recipes/recipes.dart
@@ -12,63 +12,67 @@ class Recipes extends StatelessWidget {
     final RecipesStore recipesStore = RecipesStore();
     // recipesStore.fecthRecipes();
 
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.centerLeft,
-              width: MediaQuery.of(context).size.width,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 40.0),
-                child: Text(
-                  'Recipes',
-                  style: TextStyle(
-                      color: Colors.white, fontSize: 40, fontFamily: 'Satisfy'),
+    return SafeArea(
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Container(
+                alignment: Alignment.centerLeft,
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 40.0),
+                  child: Text(
+                    'Recipes',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontFamily: 'Satisfy'),
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.orange[50],
-                // borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
-                // border: Border.all(
-                //     width: 3, color: Colors.white, style: BorderStyle.solid),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(10, 30, 10, 30),
-                child: Observer(
-                  builder: (_) {
-                    // if (recipesStore.recipes == null) {
-                    //   return Center(child: CircularProgressIndicator());
-                    // }
-                    return ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 25,
-                      itemBuilder: (BuildContext context, int index) {
-                        RecipeModel recipeTest = RecipeModel()
-                          ..title = 'Test Recipe macarron'
-                          ..image =
-                              'https://spoonacular.com/recipeImages/621398-556x370.jpg'
-                          ..score = 50.0
-                          ..healthScore = 22.5
-                          ..servings = 2
-                          ..readyInMinutes = 45
-                          ..vegetarian = true
-                          ..glutenFree = true;
-                        return RecipeCard(recipe: recipeTest);
-                      },
-                    );
-                  },
+            Expanded(
+              flex: 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.orange[50],
+                  // borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
+                  // border: Border.all(
+                  //     width: 3, color: Colors.white, style: BorderStyle.solid),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 30, 10, 30),
+                  child: Observer(
+                    builder: (_) {
+                      // if (recipesStore.recipes == null) {
+                      //   return Center(child: CircularProgressIndicator());
+                      // }
+                      return ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 25,
+                        itemBuilder: (BuildContext context, int index) {
+                          RecipeModel recipeTest = RecipeModel()
+                            ..title = 'Test Recipe macarron'
+                            ..image =
+                                'https://spoonacular.com/recipeImages/621398-556x370.jpg'
+                            ..score = 50.0
+                            ..healthScore = 22.5
+                            ..servings = 2
+                            ..readyInMinutes = 45
+                            ..vegetarian = true
+                            ..glutenFree = true;
+                          return RecipeCard(recipe: recipeTest);
+                        },
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
